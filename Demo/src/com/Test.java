ad4f1cf0c9d1f5dab844686a7562ccb6245876a5
PratikshaNirkhe <nirkhepratiksha29@gmail.com>
2023-01-25 16:47:42 +0530

    Update Test.java
    Student Data Set

--- Demo/src/com/Test.java
+++ Demo/src/com/Test.java
@@ -5,5 +5,12 @@ public class Test {
 	public static void main(String[] args) {
 
 		System.out.println("Feature Create file");
+
+		Student s = new Student();
+		s.setId(101);
+		s.setName("Rani");
+		s.setAddress("Pune");
+
+		System.out.println("Check Student Deatils:" + s);
 	}
 }
